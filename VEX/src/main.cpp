75eb4be566a1b0098ab0e3e12db5b9b7469f8c9a
Super8Aaron8 <super8aaron8@gmail.com>
2026-07-22 02:02:33 -0400

    add configuration

--- VEX/src/main.cpp
+++ VEX/src/main.cpp
@@ -11,3 +11,24 @@
 #include "vex.h"
 
 using namespace vex;
+
+// CONFIGURATION
+brain Brain;
+inertial BrainInertial = inertial();
+motor PMotor1 = motor(PORT1);
+motor PMotor2 = motor(PORT2);
+motor PMotor3 = motor(PORT3);
+motor_group PMotors = motor_group(PMotor1, PMotor2, PMotor3);
+motor SMotor = motor(PORT4);
+touchled TouchLED = touchled(PORT5);
+bumper Bumper = bumper(PORT6);
+optical Optical1 = optical(PORT7);
+optical Optical2 = optical(PORT8);
+optical Optical3 = optical(PORT9);
+optical Optical4 = optical(PORT10);
+optical Optical5 = optical(PORT11);
+
+int main() {
+
+  while (true) { wait(10, msec); }
+}
