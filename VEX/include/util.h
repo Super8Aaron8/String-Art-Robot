f509e9afae32343a79c4e79e39c98e14d71c1680
Super8Aaron8 <super8aaron8@gmail.com>
2026-07-29 22:41:02 -0400

    update utils

--- VEX/include/util.h
+++ VEX/include/util.h
@@ -1,5 +1,9 @@
 #include "main.h"
 
-int clamp(int &num, int lower, int upper);
+int clamp(int num, int lower, int upper);
+
+double dclamp(double num, double lower, double upper);
 
 int normalize(int num, int multiple, bool pos = true);
+
+double dnormalize(double num, double multiple, bool pos = true);
--- VEX/src/util.cpp
+++ VEX/src/util.cpp
@@ -1,7 +1,16 @@
 #include "util.h"
 #include "vex.h"
 
-int clamp(int &num, int lower, int upper) {
+int clamp(int num, int lower, int upper) {
+  if (num > upper) {
+    num = upper;
+  } else if (num < lower) {
+    num = lower;
+  }
+  return num;
+}
+
+double dclamp(double num, double lower, double upper) {
   if (num > upper) {
     num = upper;
   } else if (num < lower) {
@@ -16,6 +25,19 @@ int normalize(int num, int multiple, bool pos) {
       num = abs(num % multiple);
     } else {
       num = num % multiple;
+    }
+    return num;
+  } else {
+    return num;
+  }
+}
+
+double dnormalize(double num, double multiple, bool pos) {
+  if (multiple != 0) {
+    if (pos) {
+      num = fabs(fmod(num, multiple));
+    } else {
+      num = fmod(num, multiple);
     }
     return num;
   } else {
